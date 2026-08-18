20191d0eb980bfffd2f9f6dfae641b195f6b7356
lolipuru <banstellazz@gmail.com>
2026-08-18 22:56:52 +0000

    myron: update proprietary-files from OS4.0.0.15.XPMCNXM
    Change-Id: I0b7b5e6ca7b9dd5f581f39f94b4147542be3245b
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -19,12 +19,6 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
-from extract_utils.tools import (
-    llvm_objdump_path,
-)
-from extract_utils.utils import (
-    run_cmd,
-)
 
 namespace_imports = [
     'device/xiaomi/sm8850-common',
@@ -37,23 +31,6 @@ namespace_imports = [
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
 }
-
-def blob_fixup_gme_ois_read(
-    ctx: BlobFixupCtx,
-    file: File,
-    file_path: str,
-    *args,
-    **kwargs,
-):
-    with open(file_path, 'rb+') as f:
-        content = f.read()
-        target_bytes = b'\x5a\x78\x61\x0e\x7a\x02\x80\x3d\x03\x0c\x40\xfd'
-        patched_bytes = b'\x5a\x78\x61\x0e\x7a\x02\x80\x3d\x03\x08\x40\xfd'
-        if target_bytes in content:
-            content = content.replace(target_bytes, patched_bytes)
-            f.seek(0)
-            f.write(content)
-            f.truncate()
 
 blob_fixups: blob_fixups_user_type = {
     (
@@ -68,12 +45,12 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
     (
-        'odm/lib64/libMiEmojiEffect.so',
-        'odm/lib64/libMiVideoFilter.so',
         'odm/lib64/libAncHumanPreviewBokeh.so',
         'odm/lib64/libarcsoft_beautyshot.so',
+        'odm/lib64/libMiEmojiEffect.so',
+        'odm/lib64/libMiPhotoFilter.so',
+        'odm/lib64/libMiVideoFilter.so',
         'odm/lib64/libwa_widelens_undistort.so',
-        'vendor/lib64/libMiPhotoFilter.so'
     ): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
@@ -84,9 +61,6 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_isSupported'),
     (
        'odm/lib64/camera/components/com.qti.node.dewarp.so',
-       'odm/lib64/hw/com.qti.chi.override.so',
-       'odm/lib64/libcamximageformatutils.so',
-       'odm/lib64/libchifeature2.so',
        'odm/lib64/vendor.qti.hardware.camera.offlinecamera-service-impl.so',
     ): blob_fixup()
         .replace_needed(
@@ -139,23 +113,19 @@ blob_fixups: blob_fixups_user_type = {
         "odm/lib64/camera/preloadplugins/com.xiaomi.plugin.offlineyuvsplit.so",
         "odm/lib64/camera/preloadplugins/com.xiaomi.plugin.offlineyuvwarp.so",
         "odm/lib64/com.xiaomi.plugin.ecdengine.so",
-        "vendor/bin/hw/vendor.qti.camera.provider-service_64",
-        "vendor/lib64/camera/components/com.mi.node.fd.so",
-        "vendor/lib64/camera/components/com.qti.node.fd.so",
-        "vendor/lib64/com.xiaomi.stubv1.camx.so",
-        "vendor/lib64/hw/camera.qcom.core.so",
-        "vendor/lib64/libcamxdumpinforecorder.so",
-        "vendor/lib64/libmicamera_hal_core.so",
-        "vendor/lib64/libsimulation.so",
+        "odm/bin/hw/vendor.qti.camera.provider-service_64",
+        "odm/lib64/camera/components/com.mi.node.fd.so",
+        "odm/lib64/camera/components/com.qti.node.fd.so",
+        "odm/lib64/com.xiaomi.stubv1.camx.so",
+        "odm/lib64/hw/camera.qcom.core.so",
+        "odm/lib64/libcamxdumpinforecorder.so",
+        "odm/lib64/libmicamera_hal_core.so",
+        "odm/lib64/libsimulation.so",
     ): blob_fixup()
         .replace_needed(
             'libtinyxml2.so',
             'libtinyxml2-v36.so'
         ),
-    (
-       'vendor/lib64/camera/components/com.qti.node.gme.so',
-    ): blob_fixup()
-        .call(blob_fixup_gme_ois_read),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
